6e619992fcb3efc1fd4f2492760f3919297030ba
Gunpowderman <adel.baroody@gmail.com>
2021-02-22 16:53:10 +0300

    react task 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./Logo.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center" }}>The Faker's Bakers</h1>
+      <h3 style={{ textAlign: "center" }}>Get your carb fix!</h3>
+      <img alt="shop logo" src={logo} />
+    </>
   );
 }
 
